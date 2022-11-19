5e5bc04f38d6c9c1ff462a80eaef7328126afbf6
Leejin Cha <117060645+leejincha@users.noreply.github.com>
2022-11-19 19:41:25 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.06 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,6 @@
 class Solution {
     public String solution(String[] seoul) {
-        int location = 0;
-
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) {
-                location = i;
-            }
-        }
-        return ("김서방은 "+location+"에 있다");
+        String answer = "";
+        return answer;
     }
 }
